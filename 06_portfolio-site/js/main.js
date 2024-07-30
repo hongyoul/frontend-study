66de4a1d9abf60107ff4f4a56a7f43d25ee63c98
Hong Youl <hongyoul03@gmail.com>
2024-07-30 14:16:13 +0900

    2024.7.30pm14

--- 06_portfolio-site/js/main.js
+++ 06_portfolio-site/js/main.js
@@ -6,19 +6,22 @@ spyEls.forEach(function(spyEl){
 new ScrollMagic.Scene({ // 감시할 장면 추가 및 옵션 지정
   triggerElement: spyEl, // 화면에 보여짐 여부를 감시할 요소 지정
   triggerHook: 0.5 // 화면의 50% 지점에서 보여짐 여부 감시 (0~1사이 지정)
-})
-
-  .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show클래스 추가
+}).setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show클래스 추가
   .addTo(new ScrollMagic.Controller());
 });
+
 
 // 모달창 띄우기
 let modalEl = document.querySelector('#modal');
 let modalBtn = document.querySelectorAll('.port .btn-modal');
 let closeBtn = document.querySelector('#modal .btn-close');
+let modalProfileButton = document.querySelector('#phj_modalButton');
+let div_profilePopup = document.querySelector('#hj_profilePopup'); 
+let div_PopupBackGround = document.querySelector('#hj_PopupBackGround'); 
 console.log(modalBtn);
 console.log(modalBtn[0]);
 console.log(modalBtn[1]);
+console.log(modalProfileButton);
 
 modalBtn[0].addEventListener('click', function () {
   // console.log('클릭됨');
@@ -27,6 +30,15 @@ modalBtn[0].addEventListener('click', function () {
 closeBtn.addEventListener('click', function () {
   modalEl.style.display = 'none';
 });
+modalProfileButton.addEventListener('click', function(){
+  div_profilePopup.style.display = 'flex'
+  div_PopupBackGround.style.display = 'flex';
+})
+
+hj_PopupBackGround.addEventListener('click', function(){
+  div_profilePopup.style.display = 'none'
+  div_PopupBackGround.style.display = 'none';
+})
 
 
 // 현재 연도 표시
@@ -64,9 +76,9 @@ window.addEventListener('scroll', function () {
 
 
 
-// HEAD
 
 
+// 홍율
 // face는 왜? 가운데로 안올까요?
 
 
@@ -335,4 +347,4 @@ $(document).ready(function(){
   });
   
 });
-// >>>>>>> b161b4596da153d0b5deb7bd98c2a1f844f75029
+
